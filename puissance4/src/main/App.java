f266f7d2c14568d947e2878e99915cd9272feb93
Luke859 <71589052+Luke859@users.noreply.github.com>
2021-09-23 16:30:24 +0200

    ajout de la grille

--- puissance4/src/main/App.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("Welcome to our puissance 4");
-    }
-}
---
+++ puissance4/src/main/InterfaceMain.java
@@ -0,0 +1,17 @@
+public class InterfaceMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Welcome to our puissance 4");
+
+        for(int x = 0; x < 6; x++){
+
+            System.out.println('|'+ "        " + "|");
+        }
+
+        for (int y = 0; y < 10; y++){
+        System.out.print("-");
+        }
+    
+    }
+}
