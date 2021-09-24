36abac630359a071a53cc5b30f33e9645a9f10bc
Luke859 <71589052+Luke859@users.noreply.github.com>
2021-09-24 15:44:01 +0200

    mise a jour de l'interface du jeu

--- puissance4/src/main/InterfaceMain.java
+++ puissance4/src/main/InterfaceMain.java
@@ -2,15 +2,23 @@ public class InterfaceMain
 {
     public static void main( String[] args )
     {
-        System.out.println("Welcome to our puissance 4");
+        System.out.println("Welcome to our puissance 4\n");
+
+        System.out.println("Debut de la partie : ");
+
 
         for(int x = 0; x < 6; x++){
 
-            System.out.println('|'+ "        " + "|");
+            System.out.println('#'+ "        " + "#");
         }
 
         for (int y = 0; y < 10; y++){
-        System.out.print("-");
+        System.out.print("#");
+        }
+        System.out.println();
+
+        for (char v = 'a'; v <= 'h'; v++){
+            System.out.print(v);
         }
     
     }
