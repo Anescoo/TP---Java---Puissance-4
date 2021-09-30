3f2e843b41ef9d56c10312bc522cf4e601313124
Anescoo <72858178+Anescoo@users.noreply.github.com>
2021-09-30 14:13:51 +0200

    some problems need to be resolve

--- puissance4/src/main/Player.java
+++ puissance4/src/main/Player.java
@@ -8,20 +8,26 @@ public class Player {
 	final String winGame = "Vous avez gagné la partie !";
 	final String loseGame = "Vous avez perdu la partie !";
 	final String drawGame = "Egalité pour tous les joueurs";
+	 
 
 	String ChooseColumn() {
 		System.out.println(startingGame);
-		System.out.print("→ ");
+		System.out.print(">> ");
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
 		try{
 			String value = br.readLine();
+			if (value > 65 );
 			return value;
 		}
 		catch(IOException e){
 			System.err.println("tkt tkt");
 			return "veuillez réssayez";
 		}
+	}
+
+	public static Turn() {
+		for() 
 	}
 
 	public void dispStartingGame() {
