09eed8fe0c698a61b271711bbd1e4952eb194f35
Luke859 <71589052+Luke859@users.noreply.github.com>
2021-10-04 23:13:34 +0200

    ajout des tours joueur mais reste probleme à regler

--- puissance4/src/main/InterfaceMain.java
+++ puissance4/src/main/InterfaceMain.java
@@ -3,17 +3,22 @@ package main;
 public class InterfaceMain
 {
     public static void main( String[] args )
-    {
+    {   
+
         System.out.println("Welcome to our puissance 4\n");
 
         Grid g1 = new Grid(8, 6);
-        System.out.println("Debut de la partie : ");
-        System.out.println("---------------------");
-        g1.playTurn();
-        g1.dispStartingGame();
-        g1.afficheGrid();
 
-        // System.out.println("Tour du joueur" + (i%2==1 ? 'X' : 'O'));  <- tour joueur
+        for(int tour = 1; tour <= 8*6  ; tour++){
+    
+            System.out.println("\nDebut de la partie : ");
+            System.out.println("---------------------");
+            System.out.println("Tour " + tour + " :" + " Joueur " + (tour%2 == 1 ? 'O' : 'X'));
+            g1.playTurn();
+            g1.dispStartingGame();
+            g1.afficheGrid();
+
+        }
 
     }
 }
